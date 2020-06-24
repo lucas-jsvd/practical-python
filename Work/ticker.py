45a75bf665fd0cf1f44296c1b5dc4728c4f87f37
Lucas José <66851451+lucas-jsvd@users.noreply.github.com>
2020-06-23 21:28:10 -0300

    Resolução dos exercicios do capítulo 6_3
    Resolução dos exercicios do capítulo 6_3

---
+++ Work/ticker.py
@@ -0,0 +1,55 @@
+import csv
+import report
+import tableformat
+from follow import follow
+
+
+def select_colums(rows, indices):
+    for row in rows:
+        yield [row[indice] for indice in indices]
+
+
+def convert_types(rows, types):
+    for row in rows:
+        yield [func(val) for func, val in zip(types, row)]
+
+
+def make_dicts(rows, headers):
+    for row in rows:
+        yield dict(zip(headers, row))
+
+
+def filter_symbols(rows, names):
+    for row in rows:
+        if row["name"] in names:
+            yield row
+
+
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows = select_colums(rows, [0, 1, 4])
+    return rows
+
+
+def ticker(portfile, logfile, fmt):
+    portfolio = report.read_portfolio(portfile)
+    lines = follow(logfile)
+    rows = parse_stock_data(lines)
+    rows = filter_symbols(rows, portfolio)
+    formatter = tableformat.create_formatter(fmt)
+    formatter.headings(['Name','Price','Change'])
+    for row in rows:
+        formatter.row([row['name'], f"{row['price']:0.2f}", f"{row['change']:0.2f}"])
+
+
+def main(args):
+    if len(args) != 4:
+        raise SystemExit('Usage: %s portfoliofile logfile fmt' % args[0])
+    ticker(args[1], args[2], args[3])
+
+
+if __name__ == "__main__":
+    lines = follow('Data/stocklog.csv')
+    rows = parse_stock_data(lines)
+    for row in rows:
+        print(row)
